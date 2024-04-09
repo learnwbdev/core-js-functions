c84882ca43bed057d2f1a19f55a29867a5476a49
Alexandra Paramonova <alpar.wb@gmail.com>
2024-04-09 15:45:09 +0500

    feat: implement memoize

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -116,8 +116,14 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cachedRes;
+  return function cached() {
+    if (typeof cachedRes === 'undefined') {
+      cachedRes = func();
+    }
+    return cachedRes;
+  };
 }
 
 /**
